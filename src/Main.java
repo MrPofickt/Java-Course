c2d54bbb2c28ef7421e69a48fbc58c494a5f2191
Dima Vakhtin <dimavah11@gmail.com>
2024-01-18 23:40:29 +0300

    test 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        int value1 = 10;
+        int value2 = 15;
+        int result = value1 - value2;
+        System.out.println(result);
+    }
+}
