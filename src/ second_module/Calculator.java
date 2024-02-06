2ab62dfe5b42f0c2976805c43bfe3684437b0c54
Dima Vakhtin <dimavah11@gmail.com>
2024-02-06 03:13:26 +0300

    Добавление калькулятора и папки с модулями

---
+++ src/ second_module/Calculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Введите первое число: ");
+        int result = scanner.nextInt();;
+
+        while (true) {
+            System.out.print("Введите операцию '+', '-', '*', '/', 'C' для сброса, 's' для выхода: ");
+            char operation = scanner.next().charAt(0);
+
+            if (operation == 's') {
+                System.out.println("Выход из программы.");
+                break;
+            }
+
+            if (operation != 'C') {
+                System.out.print("Введите второе число: ");
+                int operand2 = scanner.nextInt();
+
+                switch (operation) {
+                    case '+':
+                        result += operand2;
+                        break;
+                    case '-':
+                        result -= operand2;
+                        break;
+                    case '*':
+                        result *= operand2;
+                        break;
+                    case '/':
+                        if (operand2 != 0) {
+                            result /= operand2;
+                        } else {
+                            System.out.println("Ошибка: деление на ноль!");
+                            return;
+                        }
+                        break;
+                    default:
+                        System.out.println("Некорректная операция");
+                        return;
+                }
+
+                System.out.println("Результат: " + result);
+            } else {
+                System.out.println("Результат сброшен.");
+                System.out.print("Введите первое число: ");
+                result = scanner.nextInt();;
+            }
+        }
+    }
+}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        int value1 = 10;
-        int value2 = 15;
-        int result = value1 - value2;
-        System.out.println(result);
-    }
-}
